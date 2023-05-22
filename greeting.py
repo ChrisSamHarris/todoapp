437296c9676b124e6cf71de82ce6d0ee66f47217
Christopher Harris <christopher.harris@moneysupermarket.com>
2023-05-22 18:23:35 +0100

    Greeting added to welcome user

---
+++ greeting.py
@@ -0,0 +1,14 @@
+import datetime 
+
+def greet():
+    dt = datetime.datetime.now().strftime('%A %d %B %Y, %I:%M%p')
+    if dt[-2:] == "AM":
+        time_greet = "Good Morning"
+    elif dt[-2:] == "PM":
+        time_greet = "Good Evening"
+    print(f"\n{time_greet}. It is {dt}.\n")
+
+
+# print(f"Todays date: {dt}")
+if __name__ == "__main__":
+    greet()
