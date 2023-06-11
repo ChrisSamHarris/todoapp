5e6acec7dd6a80e63e80cae59867f3a3b5841c85
Christopher Harris <christopher.harris@moneysupermarket.com>
2023-06-11 18:51:49 +0100

    bonus_rename: Renaming bonus dir and contents for later reference

--- BonusDir/bonus1.py
+++
@@ -1,6 +0,0 @@
-###### BONUS 1 ######
-user_title = input('Enter an article title: ')
-
-print("The title length is " + str(len(user_title)) + " characters.")
-print("The length of the title:", len(user_title))
-
--- BonusDir/bonus3.py
+++
@@ -1,9 +0,0 @@
-###### BONUS 3 ######
-x = 1
-target_num = int(input("Enter your target number: "))
-
-while x <= target_num:
-    print(x)
-    x += 1
-
-print('Finished!')
